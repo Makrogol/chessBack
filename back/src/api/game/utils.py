cde25526f68fc5482d3a816e0fe18d97ecafc5fc
Makrogol <serebryakov.kostya@gmail.com>
2025-03-28 01:14:19 +0300

    add logs for debug

--- back/src/api/game/utils.py
+++ back/src/api/game/utils.py
@@ -17,6 +17,8 @@ def parse_received_data(data: dict) -> ReceivedDataType:
 
 async def data_reaction(manager: WebSocketManager, data: dict):
     received_data_type = parse_received_data(data)
+    print("data ", data)
+    print("received_data_type ", received_data_type)
     match received_data_type:
         case ReceivedDataType.TURN_DATA: # Сама игра
             print('turn ', data)
