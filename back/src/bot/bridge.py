a250c65a8ca6e26ac6142ffb486c4ddb96705121
Makrogol <serebryakov.kostya@gmail.com>
2025-04-26 22:42:11 +0300

    add logs

--- back/src/bot/bridge.py
+++ back/src/bot/bridge.py
@@ -12,13 +12,16 @@ class Bridge:
         self.model_path = model_path
 
     def predict_move(self, fen: str) -> Tuple[Move, str]:
+        print(f"predict move for fen {fen}")
         agent = Agent(fen=fen, local_predictions=True, model_path=self.model_path)
         agent.run_simulations(n=config.SIMULATIONS_PER_MOVE)
         moves = agent.mcts.root.edges
+        print(f"predict move find all moves")
 
         sum_move_visits = sum(e.N for e in moves)
         probs = [e.N / sum_move_visits for e in moves]
 
         best_move = moves[np.argmax(probs)]
+        print(f"predict move find best move: {best_move}")
 
         return best_move.move, best_move.output_node.fen
